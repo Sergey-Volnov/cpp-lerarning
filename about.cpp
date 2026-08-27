12b00c5beb8a8fc19b9f711455647ecf8c602006
SergeyVolnov <sergalexvoln@gmail.com>
2026-08-27 12:41:53 +0400

    My first C++ programs

---
+++ about.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Sergey Volnov" << std::endl;
+	std::cout << "univercity by Korolev" << std::endl;
+	std::cout << "Tolyatti" << std::endl;
+	return 0;
+}
---
+++ first.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Linux!" << std::endl;
+    std::cout << "I am learning C++" << std::endl;
+    return 0;
+}
